7c29cf595cbfc1b00206911c1302a4074024c4be
abhi08699 <abhi08699@gmail.com>
2024-05-10 16:55:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,16 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import nltk as nltk
+nltk.download('punkt')
 from nltk.translate.bleu_score import corpus_bleu
+from nltk.translate import meteor
+from nltk import word_tokenize
+
 # Example usage:
 candidate_translation = "When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Pant Answer 3. Slip. In practically every home and workplace, there are electrical appliances and machinery. Although they are common and practical, they can also be quite dangerous. Thousands of people suffer amazed each year."
 reference_translations = ["When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."]
+reference_text = ["When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."]
+
 
 # Calculate BLEU score
 #bleu_score = calculate_bleu(candidate_translation, reference_translations)
@@ -14,3 +20,4 @@ candidate_tokens = candidate_translation.split()
 references_tokens = [ref.split() for ref in reference_translations]
 bleu_score =  corpus_bleu([references_tokens], [candidate_tokens])
 st.write("BLEU Score:", round(bleu_score,2))
+st.write("Meteor Score:",(round(meteor([word_tokenize(candidate_translation)],word_tokenize(reference_text)), 2))
