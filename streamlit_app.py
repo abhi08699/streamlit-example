a54ba1ab73bf35b3d9a0b771f750ef74c08ac6ce
abhi08699 <abhi08699@gmail.com>
2024-05-10 22:17:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,16 @@ from nltk.translate import meteor
 from nltk import word_tokenize
 
 # Example usage:
-candidate_translation = "When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Pant Answer 3. Slip. In practically every home and workplace, there are electrical appliances and machinery. Although they are common and practical, they can also be quite dangerous. Thousands of people suffer amazed each year."
-reference_translations = ["When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."]
-reference_text = "When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."
+#candidate_translation = "When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Pant Answer 3. Slip. In practically every home and workplace, there are electrical appliances and machinery. Although they are common and practical, they can also be quite dangerous. Thousands of people suffer amazed each year."
+#reference_translations = ["When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."]
+#reference_text = "When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."
+
+candidate_translation = st.text_input("candidate_translation")
+reference_translation = st.text_input("reference_translations")
 
 # Calculate BLEU score
 #bleu_score = calculate_bleu(candidate_translation, reference_translations)
 candidate_tokens = candidate_translation.split()
-references_tokens = [ref.split() for ref in reference_translations]
-bleu_score =  corpus_bleu([references_tokens], [candidate_tokens])
-st.write("BLEU Score:", round(bleu_score,2))
-st.write("Meteor Score:", round(meteor([word_tokenize(candidate_translation)],word_tokenize(reference_text)), 2))
+references_tokens = [ref.split() for ref in reference_translations[reference_translation]]
+st.write("BLEU Score:", round(corpus_bleu([references_tokens], [candidate_tokens]),2))
+st.write("Meteor Score:", round(meteor([word_tokenize(candidate_translation)],word_tokenize(reference_translation)), 2))
