ddb06f95cf226818fd09586b78be815bf4c87723
abhi08699 <abhi08699@gmail.com>
2024-05-10 22:25:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,14 @@ from nltk import word_tokenize
 #reference_translations = ["When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."]
 #reference_text = "When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."
 
-candidate_translation = st.text_input("candidate_translation")
-reference_translation = st.text_input("reference_translations")
-reference_translations = [reference_translation]
-# Calculate BLEU score
-#bleu_score = calculate_bleu(candidate_translation, reference_translations)
-candidate_tokens = candidate_translation.split()
-references_tokens = [ref.split() for ref in reference_translations]
-st.write("BLEU Score:", round(corpus_bleu([references_tokens], [candidate_tokens]),2))
-st.write("Meteor Score:", round(meteor([word_tokenize(candidate_translation)],word_tokenize(reference_translation)), 2))
+candidate_translation = st.text_area("candidate_translation")
+reference_translation = st.text_area("reference_translations")
+st.button("Calculate Metrics", type="primary")
+if st.button:
+  reference_translations = [reference_translation]
+  # Calculate BLEU score
+  #bleu_score = calculate_bleu(candidate_translation, reference_translations)
+  candidate_tokens = candidate_translation.split()
+  references_tokens = [ref.split() for ref in reference_translations]
+  st.write("BLEU Score:", round(corpus_bleu([references_tokens], [candidate_tokens]),2))
+  st.write("Meteor Score:", round(meteor([word_tokenize(candidate_translation)],word_tokenize(reference_translation)), 2))
