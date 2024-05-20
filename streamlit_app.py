5cc83a6932b1f5d11f684e851a1c51fec4d0c093
abhi08699 <abhi08699@gmail.com>
2024-05-20 14:18:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import nltk
+import evaluate
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('stopwords')
@@ -13,11 +14,6 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.translate.bleu_score import sentence_bleu
-#from nltk.translate.meteor_score import meteor_score
-# Example usage:
-#candidate_translation = "When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Pant Answer 3. Slip. In practically every home and workplace, there are electrical appliances and machinery. Although they are common and practical, they can also be quite dangerous. Thousands of people suffer amazed each year."
-#reference_translations = ["When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."]
-#reference_text = "When there is a lack of adherence between the feet and the surface of a step, it is: Answer 1. Fall Answer 2. Trip Answer 3. Slip. Electrical appliances and machinery are found in virtually every home and workplace. While they are common and convenient, they can also be quite dangerous. Thousands of people are shocked every year."
 
 candidate_translation = st.text_area("candidate_translation")
 reference_translation = st.text_area("reference_translation")
@@ -30,3 +26,5 @@ if st.button:
   st.write("Corpus BLEU Score:", round(corpus_bleu([references_tokens], [candidate_tokens]),2))
   st.write("Meteor Score:", round(meteor([word_tokenize(reference_translation)],word_tokenize(candidate_translation)), 2))
  # st.write("meteor_score Score:", round(meteor_score([references_tokens], [candidate_tokens]),2))
+  bleu = evaluate.load('bleu')
+  st.write("Evaluate blue score", round(bleu.compute(predictions=[candidate_translation], references=[reference_translation]),2))
